0f61238276bf11f3e130d0950d1cd29f6263b6fb
Mariay05 <mari.panchenko005@mail.ru>
2024-03-18 16:58:55 +0300

    выполненное

--- geocoders/simple_tree_geocoder.py
+++ geocoders/simple_tree_geocoder.py
@@ -12,10 +12,23 @@ class SimpleTreeGeocoder(Geocoder):
             self.__data = data
 
     def _apply_geocoding(self, area_id: str) -> str:
-        """
-            TODO:
-            - Сделать перебор дерева для каждого area_id
-            - В ходе перебора возвращать массив элементов, состоящих из TreeNode необходимой ветки
-            - Из массива TreeNode составить полный адрес
-        """
-        raise NotImplementedError()
+        result = self.bfs(self.__data, str(area_id))
+        return ",".join([node.name for node in result][::-1])
+
+    def bfs(self, root_children, area_id: str) -> list[TreeNode] | None:
+        result = []
+
+        for node in root_children:
+            if node.id == area_id:
+                result.append(node)
+                return result
+            if len(node.areas) != 0:
+                res_return = self.bfs(node.areas, area_id)
+                if res_return:
+                    result.extend(res_return)
+                    result.append(node)
+                    return result
+
+            full_address = ",".join(address_parts)
+            return full_address
+
